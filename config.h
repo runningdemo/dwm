7f11daa22c155b446a67908fbf89c76e007cb7d7
Demo <kidliaa@gmail.com>
2021-01-08 09:17:06 +0800

    keybinding: Modkey + h/l: navigate left and right window. Modkey + shift + h/l increase/decrease size

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 1;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 22;        /* gaps between windows */
 /* static const char *fonts[]          = { "monospace:size=10" }; */
 // static const char *fonts[] = { "BallPill-regular:size=12:antialias=true:autohint=true",
 static const char *fonts[] = { "monospace:size=12:antialias=true:autohint=true",
@@ -41,7 +41,8 @@ static const Rule rules[] = {
 	{ "Slack",  NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "flameshot",  NULL,       NULL,       0,       1,           -1 },
 	{ "zoom",  NULL,       NULL,       1 << 5,       1,           -1 },
-	{ "zoom",  NULL,       "Zoom Meeting",       0,       1,           -1 },
+	{ "Peek",  NULL,       NULL,       0,       1,           -1 },
+	{ "figma-linux",  NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
@@ -72,9 +73,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char *scratchpadcmd[] = { "kitty", "-T", scratchpadname, NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -82,12 +83,12 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_l,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_h,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
